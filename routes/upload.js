03fb1abe70a2639033094d54b4e945a1807ffc8b
Dan <UnrealDanMail@gmail.com>
2023-12-19 22:02:36 -0800

    added mode de-duplication on upload

--- routes/upload.js
+++ routes/upload.js
@@ -42,15 +42,28 @@ router.post('/', ensureAuthenticated, (req, res, next) => {
     // Process the uploaded file
     const filePath = req.file.path;
     const output = execSync(`/bin/bash -c "/usr/local/bin/vortex --silent --quick --load-save ${filePath} --json-out"`);
-    
+
     let jsonData = JSON.parse(output.toString());
     if (!jsonData.modes || jsonData.modes.length <= 0) {
       throw new Error("invalid json data");
     }
 
+    const processedModeData = new Set(); // Set to track serialized mode data
+
     for (const singlePat of jsonData.modes[0].single_pats) {
       const modeId = new mongoose.Types.ObjectId();
       const modeData = await processSaveFile(filePath, modeId.toString(), singlePat);
+
+      // Serialize modeData for comparison
+      const serializedModeData = JSON.stringify(modeData);
+
+      // Check for duplicates
+      if (processedModeData.has(serializedModeData)) {
+        continue; // Skip this iteration if duplicate is found
+      }
+
+      // Add the serialized mode data to the set
+      processedModeData.add(serializedModeData);
 
       const newMode = new Mode({
         _id: modeId,
