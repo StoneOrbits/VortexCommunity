e58749f3480d5a6af22542b0be6f8396775f7af2
Dan <UnrealDanMail@gmail.com>
2023-12-21 04:39:01 -0800

    always play

--- public/js/initLightshow.js
+++ public/js/initLightshow.js
@@ -7,16 +7,18 @@ VortexLib().then(vortexLib => {
     // the canvas ID is on the attribute and it contains the mode ID inside of it
     const canvasId = canvas.getAttribute('id');
     const lightshow = new Lightshow(vortexLib, canvasId);
+    lightshow.start();
 
-    if (tiles.length == 1) {
-      lightshow.start();
-    } else {
-      canvas.addEventListener('mouseover', () => {
-        lightshow.start();
-      });
-      canvas.addEventListener('mouseout', () => {
-        lightshow.stop();
-      });
-    }
+    //if (tiles.length == 1) {
+    //  lightshow.start();
+    //} else {
+    //  lightshow.start();
+    //  canvas.addEventListener('mouseover', () => {
+    //    lightshow.start();
+    //  });
+    //  canvas.addEventListener('mouseout', () => {
+    //    lightshow.stop();
+    //  });
+    //}
   });
 });
