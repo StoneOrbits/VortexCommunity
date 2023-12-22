b5c9d6d9619352c852086b8d2592737235fee2b1
Dan <UnrealDanMail@gmail.com>
2023-12-22 06:23:24 -0800

    lots of work

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Mode = require('../models/Mode');
 
 router.get('/:userId', async (req, res) => {
   try {
     const userId = req.params.userId;
-    const user = await User.findById(userId).populate('modes');
+    const profileUser = await User.findById(userId);
 
-    if (!user) {
+    if (!profileUser) {
       // Handle the case where the user is not found
       return res.status(404).render('404', { message: 'User not found' });
     }
 
-    //if (!req.isAuthenticated()) {
-    //  // Render public profile view
-    //  return res.render('user-profile', { user });
-    //}
+    // lookup modes for this user since populate seems broken
+    profileUser.modes = await Mode.find({ createdBy: userId });
 
     // Render the profile view, passing the user object
-    res.render('profile', { user });
+    res.render('profile', { profileUser });
   } catch (err) {
     console.error(err);
     res.status(500).render('error', { message: 'An error occurred while retrieving the user profile' });
