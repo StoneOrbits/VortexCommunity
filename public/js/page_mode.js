f0c656838416f56eac81a92d349c03880e49b516
Dan <UnrealDanMail@gmail.com>
2023-12-24 02:40:21 -0800

    dont need this

--- public/js/page_mode.js
+++ public/js/page_mode.js
@@ -1,12 +1,1 @@
 import './initLightshow.js';
-
-// Function to generate the lightshow.lol URL
-function generateLightshowUrl() {
-  const modeData = serializeModeData(JSON.stringify(mode));
-  const baseUrl = 'https://lightshow.lol/loadMode?data=';
-  return baseUrl;
-}
-
-// Setup the link on page load
-const link = document.getElementById('openOnLightshow');
-link.href = generateLightshowUrl();
