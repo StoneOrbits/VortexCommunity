7a67d6027e338ad4ac3bb618a7cd0f1953d76468
Dan <UnrealDanMail@gmail.com>
2024-03-02 23:35:20 -0800

    adjustment in firmware upload route

--- config/firmwareUpload.js
+++ config/firmwareUpload.js
@@ -7,10 +7,7 @@ const firmwareStorage = multer.diskStorage({
         cb(null, 'public/firmwares/');
     },
     filename: function(req, file, cb) {
-        // Use file original name in the filename
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        const extension = path.extname(file.originalname);
-        cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+        cb(null, file.originalname);
     }
 });
 const uploadFirmware = multer({ storage: firmwareStorage });
