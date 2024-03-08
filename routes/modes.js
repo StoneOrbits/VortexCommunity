a7d0e5042fb724fdf864d2c6b96da7c42981ee60
Dan <UnrealDanMail@gmail.com>
2024-03-08 00:24:18 -0800

    fixes

--- routes/modes.js
+++ routes/modes.js
@@ -8,17 +8,17 @@ const path = require('path');
 
 // show the main modes showcase
 router.get('/', async (req, res) => {
-    const page = req.query.page || 1;
-    const searchQuery = req.query.search;
-    var modesForCurrentPage = await Mode.find().sort({ votes: -1 }).exec();
-    // If search query is present, filter the modes based on the search criteria
-    if (searchQuery) {
-        modesForCurrentPage = modesForCurrentPage.filter(mode => {
-            return mode.name.toLowerCase().includes(searchQuery.toLowerCase());
-        });
-    }
-    // Fetch modes for the current page
-    res.render('modes', { modes: modesForCurrentPage, user: req.user, currentPage: page, search: req.query.search });
+  const page = req.query.page || 1;
+  const searchQuery = req.query.search;
+  var modesForCurrentPage = await Mode.find().sort({ votes: -1 }).exec();
+  // If search query is present, filter the modes based on the search criteria
+  if (searchQuery) {
+    modesForCurrentPage = modesForCurrentPage.filter(mode => {
+      return mode.name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+  }
+  // Fetch modes for the current page
+  res.render('modes', { modes: modesForCurrentPage, user: req.user, currentPage: page, search: req.query.search });
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -7,71 +7,50 @@ const router = express.Router();
 
 // Register Page
 router.get('/', (req, res) => {
-    res.render('register', { user: req.user }); // Added user object
+  res.render('register', { user: req.user }); // Added user object
 });
 
 // Handle Registration
 router.post('/', async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        const existingUser = await User.findOne({ $or: [{ email }, { username }] });
-        if (existingUser) {
-            req.flash('error', 'Email or username is already taken');
-            return res.redirect('/register');
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        // Generate a verification token
-        const verificationToken = crypto.randomBytes(20).toString('hex');
-
-        const newUser = new User({ username, email, password: hashedPassword, verificationToken: verificationToken });
-        await newUser.save();
-
-        // Prepare the email content
-        const emailContent = `Hi ${username}, welcome to Vortex Community! Please verify your email by clicking the following link: https://vortex.community/verify?token=${verificationToken}`;
-
-        // Send email using local system command
-        const sendMailCommand = `echo "${emailContent}" | sudo /usr/bin/mail -s "Verify Your Email" ${email} -aFrom:postmaster@vortex.community`;
-        exec(sendMailCommand, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            console.log(`Email sent to ${email}`);
-        });
-
-        req.flash('success', 'User registered successfully. You can now log in.');
-        res.redirect('/login');
-    } catch (err) {
-        req.flash('error', 'An error occurred during registration');
-        console.error("Registration error:", err);
-        res.status(500).redirect('/register');
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+      req.flash('error', 'Email or username is already taken');
+      return res.redirect('/register');
     }
-});
 
-router.get('/verify', async (req, res) => {
-    try {
-        const { token } = req.query;
-        const user = await User.findOne({ emailVerificationToken: token });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-        if (!user) {
-            req.flash('error', 'Token is invalid or has expired');
-            return res.redirect('/register');
-        }
+    // Generate a verification token
+    const verificationToken = crypto.randomBytes(20).toString('hex');
+    const encodedToken = encodeURIComponent(verificationToken);
 
-        user.emailVerified = true;
-        user.emailVerificationToken = undefined; // Clear the token after verification
-        await user.save();
+    const newUser = new User({ username, email, password: hashedPassword, verificationToken: verificationToken });
+    await newUser.save();
 
-        req.flash('success', 'Email verified successfully. You can now log in.');
-        res.redirect('/login');
-    } catch (err) {
-        console.error("Verification error:", err);
-        req.flash('error', 'An error occurred during email verification');
-        res.redirect('/register');
-    }
+    // Prepare the email content
+    const emailContent = `Hi ${username}, welcome to Vortex Community! Please verify your email by clicking the following link: https://vortex.community/verify?token=${encodedToken}`;
+
+    // Send email using local system command
+    const sendMailCommand = `echo "${emailContent}" | sudo /usr/bin/mail -s "Verify Your Email" ${email} -aFrom:postmaster@vortex.community`;
+    exec(sendMailCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`Email sent to ${email}`);
+    });
+
+    req.flash('success', 'User registered successfully. You can now log in.');
+    res.redirect('/login');
+  } catch (err) {
+    req.flash('error', 'An error occurred during registration');
+    console.error("Registration error:", err);
+    res.status(500).redirect('/register');
+  }
 });
 
 module.exports = router;
