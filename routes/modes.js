e1e23e416ea38386376383ba0d0dbe429060e0a7
Dan <UnrealDanMail@gmail.com>
2024-03-14 22:27:46 -0700

    added modes json api endpoint

--- routes/modes.js
+++ routes/modes.js
@@ -21,5 +21,34 @@ router.get('/', async (req, res) => {
   res.render('modes', { modes: modesForCurrentPage, user: req.user, currentPage: page, search: req.query.search });
 });
 
+// show the main modes showcase
+router.get('/json', async (req, res) => {
+  const page = parseInt(req.query.page || 1, 10);
+  const searchQuery = req.query.search;
+  var modesForCurrentPage = await Mode.find().sort({ votes: -1 }).exec();
+  // If search query is present, filter the modes based on the search criteria
+  if (searchQuery) {
+    modesForCurrentPage = modesForCurrentPage.filter(mode => {
+      return mode.name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+  }
+
+  // Assuming you want pagination
+  const pageSize = 10; // Or any other number of items per page you prefer
+  const totalCount = modesForCurrentPage.length;
+  const pageCount = Math.ceil(totalCount / pageSize);
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const modesOnPage = modesForCurrentPage.slice(startIndex, endIndex);
+
+  // Return the modes as JSON
+  res.json({
+    data: modesOnPage,
+    page: page,
+    pages: pageCount,
+    totalCount: totalCount
+  });
+});
+
 module.exports = router;
 
