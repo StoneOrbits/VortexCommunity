aa50676c4a65c8bfd8d5a0cb5d732385e6c9901d
Dan <UnrealDanMail@gmail.com>
2024-06-24 02:46:12 -0700

    adding mode container and upload

---
+++ models/Mode.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const ModeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  deviceType: {
+    type: String,
+    required: true
+  },
+  patternSets: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PatternSet'
+  }],
+  flags: {
+    type: Number,
+    required: true
+  },
+  dataHash: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  votes: {
+    type: Number,
+    default: 0
+  },
+  upvotedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', required: true
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+ModeSchema.index({ dataHash: 1 }, { unique: true }); // Create an index on dataHash
+
+module.exports = mongoose.model('Mode', ModeSchema);
--- routes/upload.js
+++ routes/upload.js
@@ -6,6 +6,7 @@ const path = require('path');
 const fetch = require('node-fetch');
 const router = express.Router();
 const PatternSet = require('../models/PatternSet');
+const Mode = require('../models/Mode');
 const { ensureAuthenticated } = require('../middleware/checkAuth');
 const { execSync } = require('child_process');
 const fs = require('fs');
@@ -20,41 +21,69 @@ function computeHash(data) {
 }
 
 router.post('/', ensureAuthenticated, upload.array('modeFile'), async (req, res) => {
-  const { 'g-recaptcha-response': recaptchaToken } = req.body;
-  const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY;
-  const recaptchaVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${recaptchaToken}`;
+    const { 'g-recaptcha-response': recaptchaToken } = req.body;
+    const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const recaptchaVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${recaptchaToken}`;
 
-  try {
-    const recaptchaResponse = await fetch(recaptchaVerifyUrl, { method: 'POST' });
-    const recaptchaData = await recaptchaResponse.json();
+    try {
+        const recaptchaResponse = await fetch(recaptchaVerifyUrl, { method: 'POST' });
+        const recaptchaData = await recaptchaResponse.json();
 
-    if (!recaptchaData.success) {
-      return res.status(400).send("reCAPTCHA validation failed.");
+        if (!recaptchaData.success) {
+            return res.status(400).send("reCAPTCHA validation failed.");
+        }
+
+        for (const file of req.files) {
+            const filePath = file.path;
+            const fileName = path.basename(file.originalname, path.extname(file.originalname));
+            const output = execSync(`/bin/bash -c "/usr/local/bin/vortex --silent --quick --load-save ${filePath} --json-out"`);
+            const jsonData = JSON.parse(output.toString());
+
+            if (!jsonData.modes || jsonData.modes.length <= 0) {
+                throw new Error("Invalid JSON data");
+            }
+
+            const mode = jsonData.modes[0];
+            const getDeviceTypeName = (numLeds) => {
+                switch (numLeds) {
+                    case 10: return 'Gloves';
+                    case 28: return 'Orbit';
+                    case 3: return 'Handle';
+                    case 2: return 'Duo';
+                    case 20: return 'Chromadeck';
+                    default: return 'Unknown';
+                }
+            };
+            const deviceType = getDeviceTypeName(mode.num_leds);
+            const flags = mode.flags;
+
+            // Check for existing patterns globally
+            mode.single_pats = await Promise.all(mode.single_pats.map(async (pat) => {
+                const sortedPatData = sortObjectKeys(pat);
+                const serializedPatData = JSON.stringify(sortedPatData);
+                const dataHash = computeHash(serializedPatData);
+
+                const existingPatternSet = await PatternSet.findOne({ dataHash }).exec();
+                pat.isDuplicate = !!existingPatternSet;
+
+                return pat;
+            }));
+
+            req.session.modeData = {
+                name: fileName,
+                description: '',
+                deviceType,
+                flags,
+                jsonData
+            };
+        }
+
+        res.redirect('/upload/submit');
+    } catch (error) {
+        console.error(error);
+        req.flash('error', 'An error occurred during processing');
+        res.redirect('/upload');
     }
-
-    let set = new Set();
-    let processedPatterns = []; // Initialize as an array
-    for (const file of req.files) {
-      const filePath = file.path;
-      const fileName = path.basename(file.originalname, path.extname(file.originalname));
-      const output = execSync(`/bin/bash -c "/usr/local/bin/vortex --silent --quick --load-save ${filePath} --json-out"`);
-      const jsonData = JSON.parse(output.toString());
-
-      if (!jsonData.modes || jsonData.modes.length <= 0) {
-        throw new Error("Invalid JSON data");
-      }
-
-      // Use the spread operator to concatenate arrays
-      processedPatterns = processedPatterns.concat(await preparePatternSetsForSession(jsonData, set, fileName)); // Correctly accumulate patterns
-    }
-
-    req.session.tempPats = processedPatterns;
-    res.redirect('/upload/submit');
-  } catch (error) {
-    console.error(error);
-    req.flash('error', 'An error occurred during processing');
-    res.redirect('/upload');
-  }
 });
 
 function sortObjectKeys(obj) {
@@ -119,62 +148,44 @@ async function processPatternData(data, processedSet, fileName) {
 }
 
 router.get('/submit', ensureAuthenticated, (req, res) => {
-  // Retrieve the temporary pats stored in the session
-  let tempPats = [];
-  if (req.session.tempPats) {
-    tempPats = req.session.tempPats;
-  }
+  const modeData = req.session.modeData || {};
 
-  // Render the pat selection page, passing the temporary pats for display
-  res.render('uploadSubmit', { pats: tempPats });
+  res.render('uploadSubmit', { modeData });
 });
 
 router.post('/submit', ensureAuthenticated, async (req, res) => {
-  // Destructure the submitted data; ensuring arrays for single or no selection scenarios
-  let { selectedPatternSets, patIds, patNames, patDescriptions } = req.body;
-  patIds = Array.isArray(patIds) ? patIds : [patIds];
-  patNames = Array.isArray(patNames) ? patNames : [patNames];
-  patDescriptions = Array.isArray(patDescriptions) ? patDescriptions : [patDescriptions];
-
-  const tempPats = req.session.tempPats || [];
-  let patsToSave = [];
-
-  // Map patIds to their names and descriptions
-  let nameDescriptionMap = patIds.reduce((acc, patId, idx) => {
-    acc[patId] = { name: patNames[idx], description: patDescriptions[idx] };
-    return acc;
-  }, {});
-
-  // Iterate over selectedPatternSets, ensuring it's treated as an array
-  (Array.isArray(selectedPatternSets) ? selectedPatternSets : [selectedPatternSets]).forEach(selectedIdx => {
-    const index = parseInt(selectedIdx, 10);
-    // Ensure we have a corresponding entry in nameDescriptionMap
-    if (tempPats[index] && !tempPats[index].isDuplicate && nameDescriptionMap[selectedIdx]) {
-      const { name, description } = nameDescriptionMap[selectedIdx];
-
-      const data = tempPats[index].data;
-      const dataHash = computeHash(JSON.stringify(sortObjectKeys(data)));
-
-      patsToSave.push(new PatternSet({
-        _id: new mongoose.Types.ObjectId(),
-        name: name || 'Unnamed PatternSet',
-        description: description || 'No description provided.',
-        data: data,
-        dataHash: dataHash,
-        createdBy: req.user._id,
-      }));
-    }
-  });
-
-  // Save each pat to the database
   try {
-    await Promise.all(patsToSave.map(pat => pat.save()));
-    req.flash('success', 'PatternSets successfully submitted!');
-    delete req.session.tempPats; // Clean up the session
-    res.redirect('/pats');
+    const { name, description, deviceType, flags, jsonData } = req.session.modeData;
+
+    const patternSets = jsonData.modes[0].single_pats.map(data => new PatternSet({
+      _id: new mongoose.Types.ObjectId(),
+      name: data.name || 'Unnamed PatternSet',
+      description: data.description || 'No description provided.',
+      data: data,
+      dataHash: computeHash(JSON.stringify(sortObjectKeys(data))),
+      createdBy: req.user._id,
+    }));
+
+    await Promise.all(patternSets.map(pat => pat.save()));
+
+    const mode = new Mode({
+      name,
+      description,
+      deviceType,
+      patternSets: patternSets.map(pat => pat._id),
+      createdBy: req.user._id,
+      flags: parseInt(flags, 10),
+      dataHash: computeHash(JSON.stringify(patternSets.map(pat => pat._id)))
+    });
+
+    await mode.save();
+
+    req.flash('success', 'Mode and PatternSets successfully submitted!');
+    delete req.session.modeData;
+    res.redirect('/modes');
   } catch (error) {
-    console.error('Error saving pats:', error);
-    req.flash('error', 'An error occurred while submitting your pats.');
+    console.error('Error saving modes and pats:', error);
+    req.flash('error', 'An error occurred while submitting your mode.');
     res.redirect('/upload/submit');
   }
 });
