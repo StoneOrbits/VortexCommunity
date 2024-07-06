3b925c8c6f6fb3209685e7a85d82793e6445d693
Dan <UnrealDanMail@gmail.com>
2024-07-06 03:14:06 -0700

    some work

--- public/js/autoLoader.js
+++ public/js/autoLoader.js
@@ -1,23 +1,38 @@
-var path = window.location.pathname;
-var segments = path.split("/").filter(Boolean); // Filter out empty segments
+// sub-routes that need autoloading
+var subRoutes = new Set([
+    'upload/submit',
+]);
 
-var page = segments.join('_'); // Join all segments with an underscore to form the script name
-if (page.length == 0) {
-  page = 'home';
+// Helper function to determine the script based on the route segments
+function getScriptName(path) {
+    // Split path and filter out empty segments
+    var segments = path.split("/").filter(Boolean);
+
+    if (segments.length > 1) {
+        if (subRoutes.has(path)) {
+            // Handle sub-routes dynamically
+            return 'page_' + path.replace(/\//g, '_') + '.js';
+        }
+    } else if (segments.length === 1) {
+        // handle direct routes automatically
+        return 'page_' + segments[0] + '.js';
+    }
+    // handle empty route as home
+    return 'page_home.js';
 }
-var scriptName = 'page_' + page + '.js';
-var scriptPath = '/js/' + scriptName;
 
-// if the script for this page exists then load it
-var script = document.createElement('script');
-script.src = scriptPath;
-script.type = 'module'; // Specify that the script is a module
-script.onload = function() {
-  console.log(scriptName + ' loaded successfully.');
-};
-script.onerror = function() {
-  console.log('Error loading script: ' + scriptName);
-};
+function autoLoad(path) {
+    var script = document.createElement('script');
+    script.src = scriptPath;
+    script.type = 'module'; // Specify that the script is a module
+    script.onload = function() {
+        console.log(script.src + ' loaded successfully.');
+    };
+    script.onerror = function() {
+        console.log('Error loading script: ' + script.src);
+    };
+    document.body.appendChild(script);
+}
 
-document.body.appendChild(script);
-
+// Determine the script name based on the route and autoload it
+autoLoad('/js/' + getScriptName(window.location.pathname));
