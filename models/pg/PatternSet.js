7d47b6f3a7900ccda5affcdfddaa02d4b585d4e8
Unreal-Dan <unrealdanmail@gmail.com>
2026-06-28 13:42:23 -0700

    patternsets fix

--- models/pg/PatternSet.js
+++ models/pg/PatternSet.js
@@ -7,41 +7,52 @@ const PatternSet = sequelize.define('PatternSet', {
     primaryKey: true,
     autoIncrement: true
   },
+
   name: {
     type: DataTypes.STRING,
     allowNull: false
   },
+
   description: {
     type: DataTypes.TEXT,
     allowNull: true
   },
+
   data: {
     type: DataTypes.JSONB,
     allowNull: false
   },
+
   dataHash: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true
   },
+
   votes: {
     type: DataTypes.INTEGER,
     defaultValue: 0
   },
+
   uploadDate: {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW
+  },
+
+  createdBy: {
+    type: DataTypes.INTEGER,
+    allowNull: true
   }
 }, {
   tableName: 'pattern_sets',
-  timestamps: true,
-  toJSON: {
-    virtuals: true
-  }
+  timestamps: true
 });
 
-PatternSet.virtual('_id').get(function() {
-  return this.id;
+// Optional computed alias for legacy frontend expectations
+Object.defineProperty(PatternSet.prototype, '_id', {
+  get() {
+    return this.id;
+  }
 });
 
 module.exports = PatternSet;
